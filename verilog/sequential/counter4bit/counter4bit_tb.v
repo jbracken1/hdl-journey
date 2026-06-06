9beaa593018757b4d19ddebcda88d3063674014a
jbracken1 <153126681+jbracken1@users.noreply.github.com>
2026-06-06 14:40:49 -0400

    finished 4 bit counter

---
+++ verilog/sequential/counter4bit/counter4bit_tb.v
@@ -0,0 +1,21 @@
+module counter4bit_tb;
+    reg clk;
+    reg enable;
+    reg reset;
+    wire [3:0] count;
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    counter4bit uut(.clk(clk), .enable(enable), .reset(reset), .count(count));
+
+    initial begin
+        enable = 1'b0; reset = 1'b1; #20;
+        enable = 1'b1; reset = 1'b0; #200;
+        enable = 1'b0; reset = 1'b1; #20;
+        enable = 1'b1; reset = 1'b0; #100;
+        enable = 1'b0; reset = 1'b1; #20;
+        enable = 1'b1; reset = 1'b0; #10;
+    end
+
+endmodule
